e300dee567fe4d3e95849e6db3b80b13a3a67b4a
Andreas Ekström <didair@msn.com>
2019-01-04 20:19:22 +0100

    Add installatation commands for chunkwm in installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 
+echo "### Installing/Updating Brew dependencies"
+echo "Installing chunkwm and skhd..."
+brew tap crisidev/homebrew-chunkwm
+brew install chunkwm
+brew install koekeishiya/formulae/skhd
+
+echo "### Configuring Vim"
+
 echo "Creating symbolic links for Vim..."
 ln -s "$HOME/.config/.vimrc" "$HOME/.vimrc"
 ln -sf "$HOME/.config/autoload" "$HOME/.vim/"
+
+echo "### Configuring chunkwm"
 
 echo "Creating symbolic links for ChunkWM and skhd..."
 ln -s "$HOME/.config/.chunkwmrc" "$HOME/.chunkwmrc"
 ln -s "$HOME/.config/.skhdrc" "$HOME/.skhdrc"
 
-echo "Done!"
+echo "Making chunkwm executable..."
+chmod +x $HOME/.chunkwmrc
+
+echo "### Everything was completed, if any error occured you should see them above!"
+echo "### You should now be able to open vim and run :PlugInstall to get started"
+
+echo "### To add chunkwm to startup services, run these commands"
+echo "$ brew services start chunkwm"
+echo "$ brew services start skb"
